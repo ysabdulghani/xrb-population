2207e0eee1404337d3175a4c616eb3d428a6b774
ysabdulghani <ysabdulghani@gmail.com>
2024-12-31 22:29:28 -0700

    support for different instruments and diff inputs

--- sbatch_generation_arrays.py
+++ sbatch_generation_arrays.py
@@ -1,6 +1,24 @@
 import os
 import math
 from itertools import product
+import argparse
+
+parser = argparse.ArgumentParser(
+                    prog='sbatch_generation_arrays',
+                    description='Generate job files for submission to SLURM', 
+)
+
+parser.add_argument('insturment')
+parser.add_argument('partition') 
+args = parser.parse_args()
+
+insturment = args.insturment
+partition = args.partition
+
+if partition == 'priority':
+    account = 'priority-annelohfink'
+else:
+    account = 'group-annelohfink'
 
 # Parameters
 preset_values = {
@@ -10,15 +28,15 @@ preset_values = {
     'm': [6.0, 8.0, 10.0],
     'i': [0.0, 60.0, 80.0],
     'r': [0.2, 0.5, 0.9],
-    'e': [400.0, 1000, 5000]
+    'e': [400.0, 1500.0, 10000.0]
 }
 
 # Configuration
 email = "youssef.abdulghani@student.montana.edu"
 cpus_per_task = 96
 script_path = "/home/r77m975/xrb-population/observational_effects.py"
-output_dir = "/home/r77m975/xrb-population/job_files"
-outlog_base_dir = "/home/r77m975/xrb-population/out_logs"
+output_dir = f"/home/r77m975/xrb-population/job_files_{insturment}"
+outlog_base_dir = f"/home/r77m975/xrb-population/out_logs_{insturment}"
 time_per_combination = 600  # seconds
 max_job_duration = 2 * 24 * 60 * 60  # 2 days in seconds
 
@@ -40,12 +58,12 @@ combinations_list = [
 
 # sbatch template
 sbatch_template = """#!/bin/bash
-#SBATCH --account=group-annelohfink
-#SBATCH --partition=nextgen
+#SBATCH --account={account}
+#SBATCH --partition={partition}
 #SBATCH --cpus-per-task={cpus_per_task}
 #SBATCH --mem=64G
 #SBATCH --time=2-00:00:00
-#SBATCH --job-name=xrt-simulation-job-{job_id}
+#SBATCH --job-name={insturment}-simulation-job-{job_id}
 #SBATCH --output={outlog_path}/job-{job_id}-%A_%a-%j.out
 #SBATCH --error={outlog_path}/job-{job_id}-%A_%a-%j.err
 #SBATCH --mail-user={email}
@@ -86,24 +104,27 @@ for job_id, job_combinations in enumerate(combinations_list, start=1):
 
     # Log progress to the unique progress file
     params="g=$g, T=$T, a=$a, m=$m, i=$i, r=$r, e=$e"
-    echo "Job $SLURM_JOB_ID, Task $SLURM_ARRAY_TASK_ID: Running combination $param_index: $params" >> $progress_file
+    echo "$(date), Job $SLURM_JOB_ID, Task $SLURM_ARRAY_TASK_ID: Running combination $param_index: $params" >> $progress_file
 
     # Execute the command
-    apptainer exec -c --mount 'type=bind,source=/home/r77m975/xrb-population/,destination=/home/r77m975/xrb-population' --pwd /home/r77m975/xrb-population/ /home/r77m975/heasoft_docker/heasoft-v6.34.sif python {script_path} $g $T $a $m $i $r $e xrt
+    apptainer exec -c --mount 'type=bind,source=/home/r77m975/xrb-population/,destination=/home/r77m975/xrb-population' --pwd /home/r77m975/xrb-population/ /home/r77m975/heasoft_docker/heasoft-v6.34.sif python {script_path} $g $T $a $m $i $r $e {insturment}
     """
 
     sbatch_content = sbatch_template.format(
+        account=account,
+        partition=partition,
         cpus_per_task=cpus_per_task,
         job_id=job_id,
         outlog_path=job_outlog_path,
         email=email,
         progress_log_path=job_progress_log_path,
         array_size=array_size,
+        insturment=insturment,
         commands=commands
     )
 
     # Save to file
-    filename = os.path.join(output_dir, f"job_{job_id}_arrays.sbatch")
+    filename = os.path.join(output_dir, f"job_{job_id}_arrays_{insturment}.sbatch")
     with open(filename, "w") as file:
         file.write(sbatch_content)
 
